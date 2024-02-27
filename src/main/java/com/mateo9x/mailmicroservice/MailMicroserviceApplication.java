085576c2ab39a0222826f2945fbd00b91876b4ed
Mateusz <mtpala@interia.pl>
2024-02-27 20:46:58 +0100

    init config

---
+++ src/main/java/com/mateo9x/mailmicroservice/MailMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mateo9x.mailmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mateo9x/mailmicroservice/MailMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateo9x.mailmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
